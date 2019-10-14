8cd51f46ea01c912475b2b5f1f79e1dda87d9496
sudo shuhei <aim54.tomneko.sud@icloud.com>
2019-10-14 19:30:44 +0900

    プロフィール欄作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
   end
 
   def after_sign_in_path_for(resource)
