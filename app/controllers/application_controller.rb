8bab5606fb1cf9f10b12828d6764116e63da59c9
sudo shuhei <aim54.tomneko.sud@icloud.com>
2019-10-14 18:42:21 +0900

    ユーザー周り整備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+  end
+
+  def after_sign_in_path_for(resource)
+    users_show_path
+  end
+
+  def after_sign_out_path_for(resource)
+    "/"
+  end
+  def sign_in_required
+    redirect_to new_user_session_url unless user_signed_in?
+  end
+
 end
