b3c496fc8ed2a8935f933e005a4de209772a30fc
sudo shuhei <aim54.tomneko.sud@icloud.com>
2019-11-06 22:47:31 +0900

    bootstrapはじめ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def top
+    @posts = Post.order(created_at: :desc).limit(20)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,9 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id:params[:id])
-    @request = Request.find_by(post_id: @post.id, user_id: current_user.id)
+    if user_signed_in?
+      @request = Request.find_by(post_id: @post.id, user_id: current_user.id)
+    end
   end
 
   def create
